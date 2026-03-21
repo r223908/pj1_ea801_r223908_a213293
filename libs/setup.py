222bcb89d5c4392b01b21d7dee1af71a66fd8dd8
r223908 <r223908@dac.unicamp.br>
2026-03-21 07:24:16 -0300

    upload video e att setup.py e main.py

--- libs/setup.py
+++ libs/setup.py
@@ -29,7 +29,6 @@ botao_a = Pin(5, Pin.IN, Pin.PULL_UP)
 botao_b = Pin(6, Pin.IN, Pin.PULL_UP)
 botao_c = Pin(10, Pin.IN, Pin.PULL_UP)
 
-
 # ==========================================
 # 4. Configuração do Joystick
 # ==========================================
@@ -40,30 +39,26 @@ joystick_sw = Pin(22, Pin.IN, Pin.PULL_UP)
 # ==========================================
 # 5. Configuração do Display OLED (SSD1306)
 # ==========================================
-# Usando os pinos I2C corretos da BitDogLab V7 (SDA=14, SCL=15)
+# Usando os pinos I2C0 da BitDogLab V7 (SDA=2, SCL=3)
 i2c = I2C(1, scl=Pin(3), sda=Pin(2), freq=400000)
 display = ssd1306.SSD1306_I2C(128, 64, i2c, addr=0x3C)
 display.fill(0)
 display.show()
+ATT_DISPLAY_MS = 100
 
 # ==========================================
 # 6. Configuração da Matriz de LEDs
 # ==========================================
 NUM_LEDS = 25
 np = neopixel.NeoPixel(Pin(7), NUM_LEDS)
-
 LED_MEIO = 12
-
 # Buffer circular da animação do motor
-FRAMES_MOTOR = [
-    [14, 13], [15, 16], [16, 24], [16, 23], [17, 22],
-    [18, 21], [18, 20], [18, 19], [10, 11], [8, 9],
-    [0, 8], [1, 8], [2, 7], [3, 6], [4, 6], [5, 6]
-]
+FRAMES_MOTOR = [[14, 13], [15, 16], [16, 24], [16, 23], [17, 22],
+                [18, 21], [18, 20], [18, 19], [10, 11], [8, 9],
+                [0, 8], [1, 8], [2, 7], [3, 6], [4, 6], [5, 6] ]
 
 # ==========================================
 # 7. Configuração do Buzzer
 # ==========================================
-# O Buzzer A da BitDogLab costuma ficar no GPIO 21
 buzzer = PWM(Pin(21))
 buzzer.duty_u16(0) # Garante que ele comece mudo (Volume 0)
--- src/main.py
+++ src/main.py
@@ -26,36 +26,24 @@ botao_c.irq(trigger=Pin.IRQ_FALLING, handler=trata_interrupcao_botao)
 def printOled(desvio):
     # Limpa a tela antes de desenhar os novos dados
     display.fill(0) 
-    
     # Cabeçalho
     display.text("BitDogLab V7", 16, 0)
     display.text("-" * 16, 0, 10)
-    
     # 1. Lógica para descobrir a velocidade (%) e o sentido
     if abs(desvio) <= ZONA_MORTA:
         porcentagem = 0
         texto_sentido = "Parado"
     else:
         # Fator de 0.0 a 1.0 transformado em 0 a 100
-        fator = (abs(desvio) - ZONA_MORTA) / (32768 - ZONA_MORTA)
-        porcentagem = int(fator * 100)
-        
+        porcentagem = 100* (abs(desvio) - ZONA_MORTA) / (32768 - ZONA_MORTA)
         # Trava em 100% caso o ADC do joystick passe um pouquinho do limite
-        if porcentagem > 100:
-            porcentagem = 100
-            
+        porcentagem = 100 if porcentagem>100 else porcentagem
         # Define o sentido baseado no eixo Y (se precisar inverter, é só trocar o < por > aqui)
-        if desvio < 0:
-            texto_sentido = "Horario"
-        else:
-            texto_sentido = "Anti-horario"
-
+        texto_sentido = "Horario" if desvio < 0 else "Anti-horario"
     # 2. Plota as informações calculadas
     display.text("Sentido:", 0, 25)
     display.text(texto_sentido, 0, 35) # Coloquei na linha de baixo para não cortar o texto
     display.text(f"Velocidade: {porcentagem}%", 0, 50)
-    
-    # Envia para a telinha
     display.show()
 
 def buzzerSound():
@@ -67,35 +55,31 @@ def buzzerSound():
         buzzer.duty_u16(0)       # Volume 0 (Desliga o buzzer)
 
 
+
 ultimo_tempo_pisca = utime.ticks_ms()
 ultimo_tempo_oled = utime.ticks_ms() # <-- ADICIONE ESTA LINHA
 estado_led_motor = False
-
 # Garante que todos os LEDs começam apagados
 for i in range(NUM_LEDS):
     np[i] = (0, 0, 0)
 np.write()
-
 # ==========================================
 # Ciclo Principal (Independente dos Botões)
 # ==========================================
 while True:
     # 1. Pega o tempo atual logo no início para todo mundo usar
     tempo_atual = utime.ticks_ms()
-    
-    # 2. Lemos o joystick
+    # 2. Leitura do joystick
     valor_y = joystick_y.read_u16()
     desvio = valor_y - CENTRO_JOYSTICK
-    
-    # 3. Atualizamos a tela APENAS a cada 100ms (10 vezes por segundo)
-    if utime.ticks_diff(tempo_atual, ultimo_tempo_oled) >= 100:
+    # 3. Atualizamos a tela APENAS a cada xxx ms
+    if utime.ticks_diff(tempo_atual, ultimo_tempo_oled) >= ATT_DISPLAY_MS:
         printOled(desvio)
         ultimo_tempo_oled = tempo_atual
+    np.fill((0, 0, 0))      # Limpa TODOS os LEDs da matriz de uma vez. 
 
-    # Limpa TODOS os LEDs da matriz de uma vez. 
-    np.fill((0, 0, 0))
+    buzzerSound()           # Só faz o som se o joystick for pressionado
 
-    buzzerSound()
     # 1. Zona Morta (Parado)
     if abs(desvio) <= ZONA_MORTA:
         np[LED_MEIO] = cor_travada  
@@ -103,30 +87,18 @@ while True:
     else:
         fator_velocidade = (abs(desvio) - ZONA_MORTA) / (32768 - ZONA_MORTA)
         leds_por_segundo = fator_velocidade * MAX_LEDS_POR_SEGUNDO
-        
-        if leds_por_segundo < 0.1:
-            leds_por_segundo = 0.1
-
-        # Removi o "* 2" da sua fórmula original porque 
-        # agora queremos avançar um frame inteiro a cada ciclo
-        intervalo_ms = int(1000 / leds_por_segundo)
+        leds_por_segundo = .1 if leds_por_segundo < 0.1 else leds_por_segundo
+        intervalo_ms = int(1000 / leds_por_segundo)     # intervalo para correr o frame do motor inteiro
 
         tempo_atual = utime.ticks_ms()
-        if utime.ticks_diff(tempo_atual, ultimo_tempo_pisca) >= intervalo_ms:
-            # Lógica do Buffer Circular
-            if desvio < 0:
-                # Gira para um lado (avança na lista)
+        if utime.ticks_diff(tempo_atual, ultimo_tempo_pisca) >= intervalo_ms:    # Lógica do Buffer Circular do frame do motor
+            if desvio < 0:  # Gira para um lado (avança na lista)
                 indice_frame = (indice_frame + 1) % len(FRAMES_MOTOR)
-            else:
-                # Gira para o outro lado (retrocede na lista)
+            else:   # Gira para o outro lado (retrocede na lista)
                 indice_frame = (indice_frame - 1) % len(FRAMES_MOTOR)
-                
             ultimo_tempo_pisca = tempo_atual
-
-        # Acende os LEDs correspondentes ao frame atual na cor vermelha
+        # Acende os LEDs correspondentes ao frame atual na cor selecionada pelos botões
         for led in FRAMES_MOTOR[indice_frame]:
             np[led] = cor_travada
-
-    # Envia o desenho para a matriz fisicamente
-    np.write()
-    utime.sleep(0.01)
+    np.write()          # Envia o desenho para a matriz fisicamente
+    utime.sleep(0.01)   # 10 ms
